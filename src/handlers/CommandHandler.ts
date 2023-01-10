4ab6e9d54d8392bc28be13e6c812fec88c5c80cd
loicmaes <97891718+loicmaes@users.noreply.github.com>
2023-01-10 16:43:35 +0100

    :lipstick: Update commands handler logs display

--- src/handlers/CommandHandler.ts
+++ src/handlers/CommandHandler.ts
@@ -10,11 +10,17 @@ module.exports = (bot: Bot) => {
         commands.forEach((path: string) => {
             const command: Command = require(`~/commands/${path}`)
 
-            if (!command.name) return table.addRow()
-            if (!command.description) return table.addRow()
-            if (!command.callback) return table.addRow()
+            if (!command.name) return table.addRow('?', command.description ?? '?', command.category, '🔸 ÉCHOUÉ', `Nom de la commande manquant.`)
+            if (!command.description) return table.addRow(command.name, '?', command.category, '🔸 ÉCHOUÉ', `Description de la commande manquante.`)
+            if (!command.callback) return table.addRow(command.name, command.description, command.category, '🔸 ÉCHOUÉ', `Fonction d'exécution de la commande manquante.`)
 
             bot.registerCommand(command)
+            table.addRow(command.name, command.description, command.category, '🔹 SUCCÈS')
         })
+
+        return console.log(table.toString())
     }
+
+    table.addRow('Aucune donnée !')
+    console.log(table.toString())
 }
