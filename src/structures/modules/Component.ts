f32afb97b1462ea390764043e61b125eaf18aed3
loicmaes <97891718+loicmaes@users.noreply.github.com>
2023-01-10 10:19:06 +0100

    :rocket: Component classes

---
+++ src/structures/modules/Component.ts
@@ -0,0 +1,16 @@
+export class Component {
+    constructor(customId: string) {
+    }
+}
+
+export class Modal extends Component {
+    constructor(customId: string) {
+        super(customId);
+    }
+}
+
+export class Button extends Component {
+    constructor(customId: string) {
+        super(customId);
+    }
+}
